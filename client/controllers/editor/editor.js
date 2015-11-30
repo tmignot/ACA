3ea6e53c15a2915a1332da1293602f2c368f3683
bwanlin <wanlin.brian@gmail.com>
2015-11-30 04:00:52 +0100

    preview working

--- client/controllers/editor/editor.js
+++ client/controllers/editor/editor.js
@@ -1,13 +1,26 @@
 Template.Editor.events({
 	'click #form-admin-submit-g': function(e, t){
 		e.preventDefault();
-		var bg = $('#form-g-admin-bg').val();
-		var logo = $('#form-g-admin-logo').val();
 
-		var t = HomePage.findOne();
+		var homePage = HomePage.findOne();
+		
+        var prev = {
+			bgColor: $('#form-g-admin-bg-color').val() != "" ? $('#form-g-admin-bg-color').val() : homePage.bgColor,
+			mainColor: $('#form-g-admin-main-color').val() != "" ? $('#form-g-admin-main-color').val() : homePage.mainColor,
+			logoUrl: $('#form-g-admin-logo-url').val() != "" ? $('#form-g-admin-logo-url').val() : homePage.logoUrl
+		};
 
-		HomePage.update({_id: t._id}, {$set: {color: bg, url: logo}});
-	},
+
+
+		HomePage.update({_id: homePage._id}, {$set: {bgColor: prev.bgColor, mainColor: prev.mainColor, logoUrl: prev.logoUrl}}, function(err, doc){
+    if (err) {
+        console.log("update error editor");
+    } else {
+        Router.go('Editor');
+    }
+});
+	
+    },
 	'click #preview-admin-submit-g': function(e, t){
 		e.preventDefault();
 
@@ -16,7 +29,7 @@ Template.Editor.events({
 		var preview = {
 			bgColor: $('#form-g-admin-bg-color').val() != "" ? $('#form-g-admin-bg-color').val() : homePage.bgColor,
 			mainColor: $('#form-g-admin-main-color').val() != "" ? $('#form-g-admin-main-color').val() : homePage.mainColor,
-			logoUrl: $('#form-g-admin-logo-url').val() != "" ? $('#form-g-admin-logo').val() : homePage.logoUrl
+			logoUrl: $('#form-g-admin-logo-url').val() != "" ? $('#form-g-admin-logo-url').val() : homePage.logoUrl
 		};
 
 		Session.set("preview", preview);
