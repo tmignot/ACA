3cf0ca46f7f34297eb2bdba22784ee36074054c4
bwanlin <wanlin.brian@gmail.com>
2015-12-17 22:49:48 +0100

    roles pour onglet admin router + client

--- client/controllers/helpers/navbar/navbar_menu.js
+++ client/controllers/helpers/navbar/navbar_menu.js
@@ -1,3 +1,9 @@
+Template.navbar_menu.helpers({
+	isAdmin: function(){
+		return Roles.userIsInRole(Meteor.user(), ['get','list','insert', 'update', 'remove'], 'Agents');
+	}
+});
+
 Template.navbar_menu.events({
 	'click #d_logout': function(e, t) {
 		Meteor.logout();
