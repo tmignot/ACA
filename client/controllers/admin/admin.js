80470c84f7a61ffd2ef72b5501d15743083c81a0
Thomas Mignot <tmignot@gmail.com>
2015-12-19 13:32:32 +0100

    maj user ui

--- client/controllers/admin/admin.js
+++ client/controllers/admin/admin.js
@@ -1,3 +1,7 @@
+Template.Admin.onCreated(function() {
+	this.email = new ReactiveVar(false);
+});
+
 Template.Admin.onRendered(function(){
 	if (!Meteor.user()) {
 		console.log("not logged in");
@@ -8,24 +12,45 @@ Template.Admin.onRendered(function(){
 		console.log("logged in");
 		$("#wrapper").css("padding-left", "");
 	}
+	$("#inputEmail").focus();
+});
+
+Template.Admin.helpers({
+	email: function() {
+		return Template.instance().email.get()
+	}
 });
 
 Template.Admin.events({
 	'submit form': function(e,t) {
 		e.preventDefault();
-		Meteor.call('loginMethod', e.currentTarget.inputEmail.value, function(err,res) {
-			if (res.google) {
-				Meteor.loginWithGoogle({
-						loginHint: e.currentTarget.inputEmail.value,
-						requestPermissions: ['email','https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.readonly']
+		if (!t.email.get()) {
+			Meteor.call('loginMethod', e.currentTarget.inputEmail.value, function(err,res) {
+				if (res.google) {
+					var hint;
+					if (e.currentTarget.inputEmail.value.match(/@gmail.com$/)) {
+						hint = e.currentTarget.inputEmail.value;
+					} else {
+						hint = e.currentTarget.inputEmail.value + '@gmail.com';
+					}
+					Meteor.loginWithGoogle({
+						loginHint: hint,
+						requestPermissions: [
+							'email',
+							'https://www.googleapis.com/auth/calendar',
+							'https://www.googleapis.com/auth/calendar.readonly'
+						]
 				});
-			} else {
-				Meteor.loginWithPassword(
-					e.currentTarget.inputEmail.value,
-					e.currentTarget.inputPassword.value
-				);
-			}
-		});
+				} else {
+					t.email.set(true);
+				}
+			});
+		} else {
+			Meteor.loginWithPassword(
+				e.currentTarget.inputEmail.value,
+				e.currentTarget.inputPassword.value
+			);
+		}
 	},
 	'click .logout-button': function(e,t) {
 		Meteor.logout();
--- client/controllers/dashboard/dashboard.js
+++ client/controllers/dashboard/dashboard.js
@@ -6,25 +6,26 @@ Template.Dashboard.onRendered(function() {
 	}
 	console.log(data);
 	Meteor.call('gapiGetEventList', Meteor.user()._id, data, function(e,r) {
-		console.log(r);
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'title',
-				center: 'month,agendaWeek,agendaDay',
-				right: 'prev,today,next'
-			},
-			lang: 'fr',
-			events: _.map(r.result.items, function(item) {
-				return({
-					id: item.id,
-					title: item.summary,
-					start: item.start.date? item.start.date : item.start.dateTime,
-					end: item.end.dateTime
-				});
-			})
-		});
-		$("button.fc-button").removeClass('fc-button')
-			 .removeClass('fc-state-default')
-			 .addClass('btn');
+		if (r) {
+			$('#calendar').fullCalendar({
+				header: {
+					left: 'title',
+					center: 'month,agendaWeek,agendaDay',
+					right: 'prev,today,next'
+				},
+				lang: 'fr',
+				events: _.map(r.result.items, function(item) {
+					return({
+						id: item.id,
+						title: item.summary,
+						start: item.start.date? item.start.date : item.start.dateTime,
+						end: item.end.dateTime
+					});
+				})
+			});
+			$("button.fc-button").removeClass('fc-button')
+				 .removeClass('fc-state-default')
+				 .addClass('btn');
+		}
 	});
 });
