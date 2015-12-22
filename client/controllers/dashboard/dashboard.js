c6744f0066fe4fe886dcbbb7119102684bcdf5fe
Thomas Mignot <tmignot@gmail.com>
2015-12-22 11:50:14 +0100

    maj refresh token

--- client/controllers/dashboard/dashboard.js
+++ client/controllers/dashboard/dashboard.js
@@ -4,28 +4,37 @@ Template.Dashboard.onRendered(function() {
 	data = {
 		timeMin: moment().subtract(1, 'month').toISOString()
 	}
-	console.log(data);
 	Meteor.call('gapiGetEventList', Meteor.user()._id, data, function(e,r) {
 		if (r) {
-			$('#calendar').fullCalendar({
-				header: {
-					left: 'title',
-					center: 'month,agendaWeek,agendaDay',
-					right: 'prev,today,next'
-				},
-				lang: 'fr',
-				events: _.map(r.result.items, function(item) {
-					return({
-						id: item.id,
-						title: item.summary,
-						start: item.start.date? item.start.date : item.start.dateTime,
-						end: item.end.dateTime
-					});
-				})
-			});
-			$("button.fc-button").removeClass('fc-button')
-				 .removeClass('fc-state-default')
-				 .addClass('btn');
+			if (r.error) {
+				console.log(r.error);
+				var hint = Meteor.user().emails[0].address;
+				Meteor.loginWithGoogle({
+					loginHint: hint,
+					requestPermissions: ['email', 'https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.readonly'],
+				});
+			} else {
+				console.log(r);
+				$('#calendar').fullCalendar({
+					header: {
+						left: 'title',
+						center: 'month,agendaWeek,agendaDay',
+						right: 'prev,today,next'
+					},
+					lang: 'fr',
+					events: _.map(r.result.items, function(item) {
+						return({
+							id: item.id,
+							title: item.summary,
+							start: item.start.date? item.start.date : item.start.dateTime,
+							end: item.end.dateTime
+						});
+					})
+				});
+				$("button.fc-button").removeClass('fc-button')
+					 .removeClass('fc-state-default')
+					 .addClass('btn');
+			}
 		}
 	});
 });
---
+++ client/controllers/editor/editor.js
@@ -0,0 +1,4 @@
+Template.Editor.onRendered(function() {
+	$('.side-nav li').removeClass('active');
+	$('.side-nav li.editor').addClass('active');
+});
